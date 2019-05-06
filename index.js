4ba5275af2a1c9a597f01b33d089af0e6e055ead
Michelle Wuebben <wuebbenmichelle@gmail.com>
2019-05-06 15:06:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 var recipes = ({keys:0})
+
+function updateObjectWithKeyAndValue(object, key, value){
+  
+  
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  
+}
+
+function deleteFromObjectByKey(object, key, value){
+  
+}
+
+function  destructivelyDeleteFromObjectByKey(object, key, value){
+  
+}
